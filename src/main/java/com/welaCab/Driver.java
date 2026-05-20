e491a6d01c7499a081f2cc1fed1d5080e0200ac5
sbonga <4323535@myuwc.ac.za>
2026-05-20 05:37:34 +0200

    added more files and datatbase

--- src/main/java/com/welaCab/Driver.java
+++ src/main/java/com/welaCab/Driver.java
@@ -1,5 +1,63 @@
 package com.welaCab;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "drivers")
 public class Driver {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+
+    private String name;
+    private String phoneNumber;
+    private String vehicleName;
+    private String plateNumber;
+    private boolean Available;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getVehicleName() {
+        return vehicleName;
+    }
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public boolean isAvailable() {
+        return Available;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public void setAvailable(boolean available) {
+        Available = available;
+    }
 }
---
+++ src/main/java/com/welaCab/Ride.java
@@ -0,0 +1,77 @@
+package com.welaCab;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "rides")
+public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String riderPhone;
+    private String driverPhone;
+    private String pickup;
+    private String dropOff;
+    private String status; // pending, accepted, completed
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
+    //getter methods
+    public long getId() {
+        return id;
+    }
+
+    public String getRiderPhone() {
+        return riderPhone;
+    }
+
+    public String getDriverPhone() {
+        return driverPhone;
+    }
+    public String getPickup() {
+        return pickup;
+    }
+    public String getDropOff() {
+        return dropOff;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+
+
+    //setter
+    public void setId(long id){this.id = id; }
+    public void setRiderPhone(String riderPhone) {
+        this.riderPhone = riderPhone;
+    }
+    public void setDriverPhone(String driverPhone) {
+        this.driverPhone = driverPhone;
+    }
+    public void setDropOff(String dropOff) {
+        this.dropOff = dropOff;
+    }
+    public void setPickup(String pickup) {
+        this.pickup = pickup;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+}
