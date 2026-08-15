8aa42d756128f6f2191c7aa5d5908d6f7f135f49
sbonga <4323535@myuwc.ac.za>
2026-08-15 20:07:41 +0200

    debug led to starting build from scratch

--- src/main/java/com/welaCab/Repository/RiderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.welaCab.Repository;
-
-import com.welaCab.Rider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RiderRepository extends JpaRepository<Rider ,Long> {
-    Rider findByPhoneNumber(String phoneNumber);
-}
